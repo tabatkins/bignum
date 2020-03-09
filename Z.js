30b7280734101ee3db83fde8efeebbeda6932b88
Tab Atkins-Bittner <jackalmage@gmail.com>
2020-03-09 11:03:32 -0700

    Ensure that a normalization happens after multiplication.

--- Z.js
+++ Z.js
@@ -182,6 +182,7 @@ export class Z {
 				// I have enough wiggle room that 6 or 7 additions can be done without normalizing.
 				if(thatIndex%6 == 0) this._normalize();
 			}
+			this._normalize();
 		} else {
 			// Karatsuba algorithm
 			var chunkLength = Math.ceil(thisLength > thatLength ? thisLength/2 : thatLength/2);
